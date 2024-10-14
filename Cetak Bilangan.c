7aa689e441b33f91eac249b5908b5303d37d6ea5
Bill Kent <billkent289@gmail.com>
2024-10-14 21:37:59 +0700

    Create Cetak Bilangan.c
    Mencetak bilangan berdasarkan input bilangan bulat dari user

---
+++ Cetak Bilangan.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int input;
+
+    printf("Masukkan bilangan: ");
+    scanf("%d", &input);
+
+    printf("Bilangan yang anda masukkan adalah= %d", input);
+    
+    return 0;
+
+}
